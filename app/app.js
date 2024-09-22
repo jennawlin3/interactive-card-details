09563848ac5ab6db309d3c11b361bfdec52064e2
jennawlin3 <jenniferacosta437@gmail.com>
2024-09-22 00:14:12 -0400

    updated index.html, styles.css and app.js

--- app/app.js
+++ app/app.js
@@ -18,202 +18,168 @@ const formContainer = d.querySelector("#form-container");
 const errorMsg = d.querySelectorAll(".error-msg");
 const input = d.querySelectorAll("input");
 
-const allowedKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-const allowedLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "x", "z", 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'X', 'Z', " "];
+const numberPattern = /d/;
+const namePattern = /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/g;
 
-const maskNumber = "#### #### #### ####";
-const maskMM = "##";
-const maskYY = "##"
-const maskCVC = "###"
+let cardOwner;
+let cardNumber;
+let mmField;
+let yyField;
+let cvcNumber;
 
-let cardOwner = [];
+
+inputCardHolder.addEventListener("input", (e) => {
+    let value = e.target.value; 
+
+    if(e) {
+        handleInputOwner(value);       
+    }
+});
+
+inputCardNumber.addEventListener("input", (e) => {
+    let value = e.target.value;
+
+    if(e) {
+        handleInputCard(value);
+        }     
+});
+
+inputMM.addEventListener("input", (e) => {
+    let value = e.target.value;
+
+    if(e) {
+        validateMonth(value);
+        }     
+});
+
+inputYY.addEventListener("input", (e) => {
+    let value = e.target.value;
+
+    if(e) {
+        validateYear(value);
+        }     
+});
+
+inputCVC.addEventListener("input", (e) => {
+    let value = e.target.value;
+    cvcNumber = "";
+    if(value.length === 0) {
+        inputCVC.classList.add("error");
+        errorMsg[3].classList.remove("hide");
+        cvcCard.textContent = value;
+    } else {
+        inputCVC.classList.remove("error");
+        errorMsg[3].classList.add("hide");  
+        cvcNumber = value;
+        cvcCard.textContent = value;
+        }    
+});
+
+function handleInputOwner(value) {
+    cardOwner = "";
+
+    if(value.length === 0) {
+        inputCardHolder.classList.add("error");
+        errorMsg[0].classList.remove("hide");
+        holderCard.textContent = value;
+    } else {
+        inputCardHolder.classList.remove("error");
+        errorMsg[0].classList.add("hide");
+
+        if(value.match(namePattern)) {
+            cardOwner = value;
+            holderCard.textContent = cardOwner;
+        }
+    }
+}
+
+function handleInputCard(value) {
+    cardNumber = "";
+    let card = value.match(/.{1,4}(.$)?/g);
+
+    if(value.length === 0) {
+        inputCardNumber.classList.add("error");
+        errorMsg[1].classList.remove("hide");
+    } else {
+        inputCardNumber.classList.remove("error");
+        errorMsg[1].classList.add("hide");
+
+        numberCard.textContent = card.join(" ");
+        cardNumber = value;
+        console.log(cardNumber);
+    } 
+}
+
+function validateMonth(value) {
+        mmField = "";
+
+        if(value.length === 0 || Number(value) > 12) {
+            inputMM.classList.add("error");
+            errorMsg[2].classList.remove("hide");  
+        } else {
+            inputMM.classList.remove("error");
+            errorMsg[2].classList.add("hide");  
+            mmField = value;
+            
+            if(Number(value) < 10 && Number(value) > 0) {
+                number = "";
+                value = "0" + value;
+                mmCard.textContent = value;
+            } else {
+                value = value;
+                mmCard.textContent = value;
+            }
+    }
+}
+
+function validateYear(value) {
+        yyField = "";
+
+        if(value.length === 0 || Number(value) < 24 || Number(value) > 28) {
+            inputYY.classList.add("error");
+            errorMsg[2].classList.remove("hide");  
+        } else {
+            inputYY.classList.remove("error");
+            errorMsg[2].classList.add("hide");  
+            yyField = value;
+
+            yyCard.textContent = value;
+            }
+}
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    console.log(cardOwner);
+    console.log(cardNumber.length);
+    console.log(mmField);
+    console.log(yyField);
+    console.log(cvcNumber);
+
+    /*
+    let cardOwner = [];
 let cardNumber = [];
 let mmField =  [];
 let yyField =  [];
 let cvcNumber = [];
-let inputName;
+    */
 
-
-inputCardHolder.addEventListener("keypress", (e) => {
-    /*if(e.key === 'tab') {
-        return;
-    }*/
-    let cardHolderValue = e.value;
-
-    if(e) {
-        if(inputCardHolder.classList.contains("error")) {
-        inputCardHolder.classList.remove("error");
-        errorMsg[0].classList.add("hide");
-        } else {
-        handleInputOwner(e.key, cardOwner);
-        inputName = "Card Holder";
-        //return cardOwner;  
-        }        
-    }
-});
-
-inputCardNumber.addEventListener("keydown", (e) => {
-    if(e.key === 'tab') {
-        return;
-    }
-
-    if(e) {
-        if(inputCardNumber.classList.contains("error")) {
-        inputCardNumber.classList.remove("error");
-        errorMsg[1].classList.add("hide");
-        }        
-    }
-
-    e.preventDefault();
-    handleInput(maskNumber, e.key, cardNumber, numberCard);
-    inputCardNumber.value = cardNumber.join("");
-    return cardNumber;
-});
-
-inputMM.addEventListener("keydown", (e) => {
-
-    if(e.key === 'tab') {
-        return;
-    }
-
-    if(e) {
-        if(inputMM.classList.contains("error")) {
-        inputMM.classList.remove("error");
-        errorMsg[2].classList.add("hide");
-        }        
-    }    
-
-    e.preventDefault();
-    handleInput(maskMM, e.key, mmField, mm);
-
-    inputMM.value = mmField.join("");
-    validateDate(mmField)
-    return mmField.join("");
-});
-
-
-inputYY.addEventListener("keydown", (e) => {
-    if(e.key === 'tab') {
-        return;
-    }
-
-    if(e) {
-        if(inputYY.classList.contains("error")) {
-        inputYY.classList.remove("error");
-        errorMsg[2].classList.add("hide");
-        }        
-    }
-
-    e.preventDefault();
-    handleInput(maskYY, e.key, yyField, yyCard);
-    inputYY.value = yyField.join("");
-    validateDate(yyField);
-    return yyField;
-});
-
-inputCVC.addEventListener("keydown", (e) => {
-    if(e.key === 'tab') {
-        return;
-    }
-
-    if(e) {
-        if(inputCVC.classList.contains("error")) {
-        inputCVC.classList.remove("error");
-        errorMsg[3].classList.add("hide");
-        }        
-    }
-
-    e.preventDefault();
-    handleInput(maskCVC, e.key, cvcNumber, cvcCard);
-    inputCVC.value = cvcNumber.join("");
-    return cvcNumber;
-});
-
-function handleInputOwner(key, arr) {
-
-    if(key === 'Backspace' && arr.length > 0) {
-        arr.pop();
-        output.textContent = arr.join("");
-        return arr;
-    }
-
-    if(allowedLetter.includes(key)) {
-        arr.push(key);
-        output.textContent = arr.join("");
+    if(cardOwner !== [] && cardOwner.length > 4 && cvcNumber !== [] && cvcNumber.length === 3 && mmField !== [] && yyField !== [] && yyField.length === 2 && cardNumber !== [] && cardNumber.length === 16) {
+        successMsg.classList.remove("hide");
+        formContainer.classList.add("hide");
+        form.reset();
     } else {
-        return;
+        errorDiv();
     }
-
-    if(inputName === "Card Holder") {
-        inputCardHolder.textContent = arr.join("");
-    }
-    return arr;
-}
-
-function handleInput(mask, key, arr, output) {
-
-    if(key === 'Backspace' && arr.length > 0) {
-        arr.pop();
-        output.innerHTML = arr.join("");
-        console.log(arr);
-        return arr;
-    }
-
-    if(allowedKeys.includes(key) && arr.length + 1 <= mask.length) {
-        if(mask[arr.length] === "/" || mask[arr.length] === " ") {
-            arr.push(mask[arr.length], key);
-        } else {
-            arr.push(key);
-            if(output) {
-              output.innerHTML = arr.join("");   
-            }
-        }       
-    } 
-    console.log(arr.length);
-    return arr;
-}
-
-function validateDate(arr) {
-    let array = arr.join("").toString()
-
-    if(arr == mmField) {
-        if(Number(array) >= 1 && Number(array) <= 12) {
-            inputMM.classList.remove("error");
-            return arr
-        } else {
-            console.log("caso 2")
-            deleteCardDate(arr);
-        }
-    }
-    // Limit on years digits (24/28)
-    if(arr == yyField) {
-        if(Number(array) >= 24 && Number(array) <= 28) {
-            inputYY.classList.remove("error");
-            return arr;
-        } else {
-            deleteCardDate(arr);
-        }        
-    }
-}
-
-function deleteCardDate(arr) {
-    if(arr === mmField) {
-        mmCard.innerHTML = "";
-        inputMM.classList.add("error");
-    }
-    if(arr == yyField) {
-        yyCard.innerHTML = "";
-        inputYY.classList.add("error");
-    }
-}
+});
 
 function errorDiv() {
-    if(inputCardHolder.value < 1) {
+    if(inputCardHolder.value.length === 0) {
         errorMsg[0].classList.remove("hide");
         inputCardHolder.classList.add("error");
     } 
 
-    if(inputCardNumber.value.length < 1) {
+    if(inputCardNumber.value.length < 16) {
         errorMsg[1].classList.remove("hide");
         inputCardNumber.classList.add("error");
     }
@@ -228,28 +194,11 @@ function errorDiv() {
         inputYY.classList.add("error");
     } 
 
-    if(inputCVC.value.length === 0) {
+    if(inputCVC.value.length < 3) {
         errorMsg[3].classList.remove("hide");
         inputCVC.classList.add("error");
     } 
 }
-
-submitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    if(cardOwner !== [] && cardOwner.length <= 40 && cvcNumber !== [] && cvcNumber.length === 3 && mmField !== [] && mmField.length === 2 && yyField !== [] && yyField.length === 2 && cardNumber !== [] && cardNumber.length === 19) {
-        successMsg.classList.remove("hide");
-        formContainer.classList.add("hide");
-        form.reset();
-    } 
-
-    if(cardOwner.length > 30) {
-        inputCardHolder.classList.add("error");
-        cardOwner = [];
-        inputCardHolder.value = "";
-    }
-    errorDiv()
-});
 
 continueBtn.addEventListener("click", (e) => {
     if(e.target) {
